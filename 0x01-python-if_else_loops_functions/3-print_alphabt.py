db3068a9b3d35e86ee217434a1c6664b4208cd02
Ugoo <ugonnaezeabara@gmail.com>
2022-08-23 13:37:49 -0700

    print numbers

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,8 +2,8 @@
 
 number = 97
 
-while number < 123 :
-    if number == 101 or number == 113 :
+while number < 123:
+    if number == 101 or number == 113:
         number += 1
         continue
     print("{:s}".format(chr(number)), end='')
