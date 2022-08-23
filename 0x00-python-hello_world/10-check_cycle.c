b4024b2a128a03ab8b8a914e437e0b6c5b440c02
Ugoo <ugonnaezeabara@gmail.com>
2022-08-22 21:54:53 -0700

    technical interview solution (linked list)

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * check_cycle - checks if singly linked list is a cycle
@@ -16,7 +15,7 @@ int check_cycle(listint_t *list)
 	while (1)
 	{
 		/*Traverse through nodes as long as linked list node exist*/
-		if (fast->next !NULL && fast->next-> !NULL)
+		if (fast->next !NULL && fast->next->next !NULL)
 		{
 			fast = fast->next->next;
 			slow = slow->next;
@@ -28,5 +27,5 @@ int check_cycle(listint_t *list)
 		{
 			return (0);
 		}
-	} 
+	}
 }
