4240bf2e3c2d6ccf7a19dde146ec894568b4e1c2
Ugoo <ugonnaezeabara@gmail.com>
2022-08-23 13:34:00 -0700

    print numbers

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+number = 97
+
+while number < 123 :
+    if number == 101 or number == 113 :
+        number += 1
+        continue
+    print("{:s}".format(chr(number)), end='')
+    number += 1
