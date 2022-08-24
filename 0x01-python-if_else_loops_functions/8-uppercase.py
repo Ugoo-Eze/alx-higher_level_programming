0e5edbf96abb0f5947d0e26e4c9f37e9f90f5381
Ugoo <ugonnaezeabara@gmail.com>
2022-08-24 03:25:24 -0700

    function to print out words uppercased

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,7 @@
 
 def uppercase(str):
     for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-        else:
-            print("{:s}".format(i), end='')
-    print('')
+        if (ord(i) >= ord('a')) and (ord(i) <= ord('z')):
+            i = chr(ord(i)-ord('a')+ord('A'))
+        print("{}".format(i), end='')
+    print()
