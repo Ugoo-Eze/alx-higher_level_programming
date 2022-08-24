44031cfa1c6ad1327197c02fa54ec30b2a1c2a3c
Ugoo <ugonnaezeabara@gmail.com>
2022-08-24 05:22:44 -0700

    made changes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
+no = number
 
-if number < 0:
-    remainder = number % (-10)
-else:
-    remainder = number % 10
+if num < 0:
+    num = num * -1
 
-if remainder > 5:
-    print("Last digit of {} is {} and is greater than 5"
-          .format(number, remainder))
-elif remainder is 0:
-    print("Last digit of {} is {} and is 0"
-          .format(number, remainder))
-else:
-    print("Last digit of {} is {} and is less than 6 and not 0"
-          .format(number, remainder))
+last_digit = num % 10
+
+if last_digit > 5 and number < 0:
+    last_digit *= -1
+    print(f"Last digit of {number:d} is {last_digit:d}"
+          f" and is less than 6 and not 0")
+elif last_digit > 5 and number > 0:
+    print(f"Last digit of {number:d} is {last_digit:d}"
+          f" and is greater than 5")
+elif last_digit == 0:
+    print(f"Last digit of {number:d} is {last_digit:d} and is 0")
+elif last_digit < 6 and last_digit != 0 and number < 0:
+    last_digit *= -1
+    print(f"Last digit of {number:d} is {last_digit:d}"
+          f" and is less than 6 and not 0")
+elif last_digit < 6 and last_digit != 0 and number > 0:
+    print(f"Last digit of {number:d} is {last_digit:d}"
+          f" and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 
-number = 0
-
-while number <= 99:
-    if number != 99:
-        print("{02:d}, ".format(number), end='')
-    else:
-        print("{02:d}".format(number))
-    number = number + 1
+for i in range(99):
+    print("{:02d}".format(i), end=', ')
+print(99)
