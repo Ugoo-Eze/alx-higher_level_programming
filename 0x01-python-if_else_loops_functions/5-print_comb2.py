d0fd8aa73f068edb2a50e742cdfb7cb7bc92eb8f
Ugoo <ugonnaezeabara@gmail.com>
2022-08-24 00:47:59 -0700

    print numbers 0 - 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,16 +2,9 @@
 
 number = 0
 
-#for number in range(100):
-
-#    if number <= 9:
-#        print("{:d} ".format(0), end='')
-#        
-#    print("{:02d}".format(number), end='')
-
 while number <= 99:
     if number != 99:
-        print("{:02d}, ".format(number), end='')
+        print("{02:d}, ".format(number), end='')
     else:
-        print("{:02d}".format(number))
+        print("{02:d}".format(number))
     number = number + 1
