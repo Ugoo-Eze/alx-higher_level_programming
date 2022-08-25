7644eb497303362a206d0d9d7876133473170c01
Ugoo <ugonnaezeabara@gmail.com>
2022-08-25 08:47:36 -0700

    program that imports custom functions

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+add_result = add(a, b)
+sub_result = sub(a, b)
+mul_result = mul(a, b)
+div_result = div(a, b)
+
+print('{0} + {1} = {2}'.format(a, b, add_result))
+print('{0} + {1} = {2}'.format(a, b, sub_result))
+print('{0} + {1} = {2}'.format(a, b, mul_result))
+print('{0} + {1} = {2}'.format(a, b, div_result))
