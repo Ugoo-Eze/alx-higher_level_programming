92f0a13cc86387f33f4020c38a39f73a8ea0a836
Ugoo Eze <89856062+Ugoo-Eze@users.noreply.github.com>
2022-08-25 21:48:52 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print('{0} + {1} = {2}'.format(a, b, add(a, b))i)
+    print('{0} + {1} = {2}'.format(a, b, add(a, b)))
     print('{0} + {1} = {2}'.format(a, b, sub(a, b)))
     print('{0} + {1} = {2}'.format(a, b, mul(a, b)))
     print('{0} + {1} = {2}'.format(a, b, div(a, b)))
