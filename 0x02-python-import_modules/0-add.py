468d83e9109d9e62d6603ac5a7455254f53113fb
Ugoo <ugonnaezeabara@gmail.com>
2022-08-25 08:24:05 -0700

    program that imports custom function (add)

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
 
-result = add(a, b)
-print("{0} + {1} = {2}\n".format(a, b, result), end='')
+    print("{0} + {1} = {2}\n".format(a, b, result), end='')
