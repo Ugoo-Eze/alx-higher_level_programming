3054379d8bbd1984206b28dce99ee0c51a1dbbfb
Ugoo <ugonnaezeabara@gmail.com>
2022-08-25 08:51:15 -0700

    program that imports custom functions

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,12 +3,8 @@ from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
-add_result = add(a, b)
-sub_result = sub(a, b)
-mul_result = mul(a, b)
-div_result = div(a, b)
 
-print('{0} + {1} = {2}'.format(a, b, add_result))
-print('{0} + {1} = {2}'.format(a, b, sub_result))
-print('{0} + {1} = {2}'.format(a, b, mul_result))
-print('{0} + {1} = {2}'.format(a, b, div_result))
+print('{0} + {1} = {2}'.format(a, b, add(a, b)))
+print('{0} + {1} = {2}'.format(a, b, sub(a, b)))
+print('{0} + {1} = {2}'.format(a, b, mul(a, b)))
+print('{0} + {1} = {2}'.format(a, b, div(a, b)))
