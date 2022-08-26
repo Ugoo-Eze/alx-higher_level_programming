8e038c27cf843176157ff53793f139b41fb4ba85
Ugoo <ugonnaezeabara@gmail.com>
2022-08-25 22:00:28 -0700

    print all name from file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import hidden_4
-
     for i in dir(hidden_4):
-        if not (i[0] == '_' and i[1] == '_'):
-            print(i)
+        if not i.startswith("__"):
+            print("{:s}".format(i))
