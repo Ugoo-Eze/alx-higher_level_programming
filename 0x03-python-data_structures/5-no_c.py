83505e626815f3bbe020de0d38b7722890468ac5
Ugoo Eze <89856062+Ugoo-Eze@users.noreply.github.com>
2022-08-28 12:39:41 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 
 def no_c(my_string):
     for i in my_string:
-        new_string = my_string.translate({ord('C') or ord('c'): None})
+        new_string = my_string.translate({ord('C'): None or ord('c'): None})
         return new_string
