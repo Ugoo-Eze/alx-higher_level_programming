f6d2cb96b0f677ba4e4835dc474b308e8159b670
Ugoo <ugonnaezeabara@gmail.com>
2022-09-26 09:25:16 -0700

    class inherits from list

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+module 0-lookup
+
+Contains method to lookup the return list of object's attribute and method
+"""
+
+
+def lookup(obj):
+    """Returns list of object's attribute and methods"""
+    return dir(obj)
